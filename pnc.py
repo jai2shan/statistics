4700b51716b30dbc11ca4d3ca8c07b62764f74b9
jai2shan <muralis2raj@gmail.com>
2021-06-30 13:35:08 +0530

    add pnc2

--- pnc.py
+++ pnc.py
@@ -3,7 +3,10 @@ from functools import reduce
 
 
 def fact(n):
-    return reduce(mul, list(range(1, n + 1)))
+    if n == 0:
+        return 1
+    else:
+        return reduce(mul, list(range(1, n + 1)))
 
 def ncr(n,r):
     return fact(n)/(fact(r)*fact(n-r))
