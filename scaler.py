9b01f7c50cdb3903cf4d6c351cff5e141f41d7c1
jai2shan <muralis2raj@gmail.com>
2021-08-22 20:00:20 +0530

    resync

---
+++ scaler.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+import os
+
+data = pd.read_csv(r'C:\Users\jayasans4085\ARCADIS\Data Science & Advanced Analytics (Internal) - Documents\Projects\03_AR Exposure\20190522_AR Use Case\7. Data for Python\20190724_Train.csv')
